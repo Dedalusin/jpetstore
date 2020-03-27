7eca2352ecb45c64a48ff0048dacab591790750d
Dedalusin <18374850057@163.com>
2020-03-27 17:49:20 +0800

    finish domain and part of persistence and mapper

--- src/main/java/org/csu/jpetstore/JpetstoreApplication.java
+++ src/main/java/org/csu/jpetstore/JpetstoreApplication.java
@@ -1,9 +1,11 @@
 package org.csu.jpetstore;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("org.csu.jpetstore.persistence")
 public class JpetstoreApplication {
 
     public static void main(String[] args) {
