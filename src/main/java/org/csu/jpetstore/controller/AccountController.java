7220faedf8744ad17c68465ac16a68c010c0e390
liujianyu <1173770418@qq.com>
2020-04-05 14:12:32 +0800

    简单的密码加密

--- src/main/java/org/csu/jpetstore/controller/AccountController.java
+++ src/main/java/org/csu/jpetstore/controller/AccountController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import sun.misc.BASE64Encoder;
 
 import java.util.List;
 
@@ -31,7 +32,9 @@ public class AccountController {
     @PostMapping("signon")
     public String signon(String username, String password, Model model)
     {
-        Account loginAccount = accountService.getAccount(username, password);
+        BASE64Encoder encoder = new BASE64Encoder();
+        String password1 =  encoder.encode(password.getBytes());
+        Account loginAccount = accountService.getAccount(username,password1);
         if(loginAccount == null){
             String msg = "Invalid username or password.  Signon failed.";
             model.addAttribute("msg",msg);
@@ -79,6 +82,9 @@ public class AccountController {
     //注册
     @PostMapping("newAccount")
     public String newAccount(Account registerAccount,Model model){
+        BASE64Encoder encoder = new BASE64Encoder();
+        String password = encoder.encode(registerAccount.getPassword().getBytes());
+        registerAccount.setPassword(password);
         accountService.insertAccount(registerAccount);
         model.addAttribute("authenticated",true);
         model.addAttribute("account",registerAccount);
