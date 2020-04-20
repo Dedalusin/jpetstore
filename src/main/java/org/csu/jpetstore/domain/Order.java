de3472036dbf46c000d4359d64aa7fff7d985315
Kizna1ver <6563088+Kiznaiver@user.noreply.gitee.com>
2020-04-20 19:27:15 +0800

    改bug和上传前端页面

--- src/main/java/org/csu/jpetstore/domain/Order.java
+++ src/main/java/org/csu/jpetstore/domain/Order.java
@@ -37,7 +37,7 @@ public class Order implements Serializable {
   private String cardType;
   private String locale;
   private String status;
- private int sendstatus;
+  private int sendstatus;
   private boolean confirmed=false;
   private List<LineItem> lineItems = new ArrayList<LineItem>();
   public void setConfirmed(){confirmed=true;}
